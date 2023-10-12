ab6ec06eb1389e20245a139d737ada75066a976b
suhyeonOh <sh020119@snu.ac.kr>
2023-10-12 16:28:37 +0900

    공사 중지

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,5 +10,11 @@ import { Header } from '../component/Header';
 import { Footer } from '../component/Footer';
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="Home">
+      <p></p>
+      <p>next.js 공부가 아직 미진하여 추후에 공사 예정입니다....</p>
+      <p></p>
+    </div>
+  );
 }
